f734cb39e55f1b0624f9bf596c90c44bdabf9ba1
NondescriptArchetype <31418839+NondescriptArchetype@users.noreply.github.com>
2018-01-28 13:52:42 -0600

    Complete

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//A husband has been tasked with rennovating his basement per the request of his wife. In order to find out how much drywall he will need, he needs to figure out the total
+//area of the walls in his basement. Assign the area of the outer walls as a constant called outsideWall, a constant with the area of the insideWall, and then the area of the
+//floor and name it floorArea. The values will be in feet
+
+let outsideWall = 34
+let insideWall = 22
+let floor = 80
+
+let totalArea = outsideWall+insideWall+floor
+
+//Three friends are all trying to lose weight together and so they are entering their weights into an app that shows all of them their progress at the same time. Name each of the variable tom, dick, and harry and give them any weight between 170 to 250. The values will be in pounds
+
+var tom = 203
+var dick = 180
+var harry = 231
+
+var averageWeight = (tom+dick+harry)/3
